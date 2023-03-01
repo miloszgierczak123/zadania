4fea2c474166fcde87642d8a13789f01259ca962
jonathankuminga <126552445+miloszgierczak123@users.noreply.github.com>
2023-03-01 12:38:12 +0100

    Add files via upload

---
+++ zadanie_4/zadanie_4/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.Write("Podaj kwotę w złotówkach: ");
+double kwota = Convert.ToDouble(Console.ReadLine());
+double kurs = 4.41;
+double wynik = kurs * kwota;
+Console.WriteLine("Oto kwota przekonwertowana na dolary: " + wynik);
