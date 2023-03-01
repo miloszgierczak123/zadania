3d2d65078bb28dfacdb668bd180a56cacc756754
jonathankuminga <126552445+miloszgierczak123@users.noreply.github.com>
2023-03-01 09:25:55 +0100

    Add files via upload

---
+++ zadanie1/zadanie1/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+string slowo = File.ReadAllText(@"D:\test_mil_gie.txt");
+int count = 0;
+
+foreach (char x in slowo)
+{
+    if (x == 'a')
+    {
+        count++;
+    }
+}
+
+Console.WriteLine("Litera a występuje w pliku " + count + " razy.");
+
