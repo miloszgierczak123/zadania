7803b4a905e705147cb9e0d4d83aeaf215e48971
jonathankuminga <126552445+miloszgierczak123@users.noreply.github.com>
2023-03-01 09:27:31 +0100

    Add files via upload

---
+++ zadanie_2/zadanie_2/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Microsoft.VisualBasic;
+
+Console.WriteLine("Podaj ścieżkę pliku tekstowego: ");
+string sciezka = Console.ReadLine();
+string tekst = File.ReadAllText(@sciezka);
+string zmienionyTekst = tekst.Replace("praca", "job");
+Console.WriteLine(zmienionyTekst);
+DateTime dzisiaj = DateTime.Now;
+string data = dzisiaj.ToString("yyyyMMdd");
+File.WriteAllTextAsync(sciezka + "_changed" + data + ".txt", zmienionyTekst);
